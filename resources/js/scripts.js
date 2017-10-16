f29c9709b16191409b8e17eed103640ce75b2a8b
David Murphy <djmgeneseo@gmail.com>
2017-10-15 23:39:59 -0400

    Add internal links to buttons, and scroll animations via jQuery , edit media queries to make sticky nav bar more responsive and add back to top option onto navbar

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    
-    /* Sticky navigation */
+    /*------ Sticky navigation ------*/
     $('#section-features').waypoint(function(direction) {
        if (direction === 'down') {
            $('nav').addClass('sticky');
@@ -11,13 +11,39 @@ $(document).ready(function() {
            offset: '70px;'
     });
     
-    /* Scroll-on buttons */
+    /*------ Scroll-on buttons ------*/
     $('#hungry-button').click(function () {
         $('html, body').animate({scrollTop: $('.section-plans').offset().top}, 1000)
     });
     
     $('#show-more-button').click(function () {
         $('html, body').animate({scrollTop: $('.section-features').offset().top}, 1000)
+    });
+    
+    /*------ Navigation Scroll ------*/
+    
+    
+    // Add smooth scrolling to all links
+    $("a[href^='#']").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
     });
     
 });
